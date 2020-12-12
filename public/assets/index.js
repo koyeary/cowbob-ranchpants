b5374cf8cf24f716a59b8ed8b8f38e2b904f1e96
koyeary <katyeary@gmail.com>
2020-12-11 20:17:44 -0800

    add download button on resume page and window.open to links

--- public/assets/index.js
+++ public/assets/index.js
@@ -4,22 +4,43 @@ $(window).on('scroll', function () {
         $('.top').css('background-color', 'white');
         $('.top').addClass('nav-color');
         $('.main').css('color', '#0071eb');
+        //CONTACT IF ELSE
 
     } else {
 
         $('.top').css('background-color', 'transparent');
         $('.top').removeClass('nav-color');
         $('.main').css('color', '#ffffff');
+        //CONTACT IF ELSE
     }
 
 });
 
 $('#contact-goto').on('click', function () {
-        $('body').animate({
-            scrollTop: eval($('#' + $(this).attr('target')).offset().top - 70)
-        }, 1000);
-    });
+    $('body').animate({
+        scrollTop: eval($('#' + $(this).attr('target')).offset().top - 70)
+    }, 1000);
+});
 
+$('#github').on('click', function () {
+    window.open('https://github.com/koyeary');
+} )
+
+$('#linkedin').on('click', function () {
+    window.open('https://www.linkedin.com/in/kat-yeary/');
+})
+
+//Nodemailer request
+const postReq = (name, email, message) => {
+    $.ajax('/mail', {
+        method: 'POST',
+        data: {
+            name: name,
+            email: email,
+            message: message,
+        },
+    });
+}
 
 // Send the POST request.
 $('.mail').on('click', (e) => {
@@ -29,24 +50,20 @@ $('.mail').on('click', (e) => {
     const email = $('#email').val().trim();
     const message = $('#message').val().trim();
 
-    $.ajax('/mail', {
-        method: 'POST',
-        data: {
-            name: name,
-            email: email,
-            message: message
-        }
-    }).then(
-        function () {
-            console.log('email sent');
-            // Reload the page to get the updated list
-            $('#contact').modal('hide');
-        }
-    );
+    $('#contact').modal('hide');
+    postReq(name, email, message);
+    alert('Message sent');
+    location.reload();
+    /*             if(!this.form.checkbox.checked)
+                {
+                    alert('You must agree to the terms first.');
+                    return false;
+                } else { */
+    // Reload the page to get the updated list
+    // $('#contact').modal('hide');
 
-    if ($("#signUp").checked = true) {
-        console.log('checked');
-    }
+    //}
+    //);
 });
 
 
--- routes.js
+++ routes.js
@@ -3,9 +3,11 @@ module.exports = function(app){
     const application = require('./routes/application');
     const mail = require('./routes/transporter');
     const resume = require('./routes/resume');
+    const failure = require('./routes/failure');
 
     app.use('/', application);
     app.use('/mail', mail);
     app.use('/resume', resume);
+    app.use('/failure', failure);
 
 }
